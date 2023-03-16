bdd25a14d7edd31cfe969eebd543968945ec3e43
Aldo Buzzo <aldo_buzzo@it.ibm.com>
2023-03-16 09:47:36 +0100

    project initialization

---
+++ src/main/java/com/ibm/artsanadeq/IotBbcCeDequeueApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.artsanadeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotBbcCeDequeueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotBbcCeDequeueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/artsanadeq/IotBbcCeDequeueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.artsanadeq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotBbcCeDequeueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
